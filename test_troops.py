d8754050bad299e6ae5232b64e8374f678d0b13e
jujga <jujgabk@gmail.com>
2022-02-12 12:37:08 +0200

    перевел шаги на Enum

--- test_troops.py
+++ test_troops.py
@@ -9,7 +9,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class WebElements(Enum):
     USERNAME_FIELD = (By.ID, "_username")
-
+    USERPASSWORD_FIELD = (By.ID, "_password")
+    SE_BUTTON = (By.XPATH,"//button[@class='primary large']")
+    TALENTS_LINK = (By.XPATH, "//li[@data-testid = 'TEACandidatesListLink']")
+    POPUP_TITLE = (By.XPATH, "//div[contains(@class,'text') and contains(@class,'ng-tns')]")
     def __init__(self, location_strategy, locator):
         self.location_strategy = location_strategy
         self.locator = locator
@@ -25,23 +28,22 @@ class Credentials:
 def firefox_driver():
     driver = webdriver.Firefox(executable_path=firefox.GeckoDriverManager().install())
     yield driver
-    driver.close()
+    #driver.close()
 
 def test_login(firefox_driver):
     firefox_driver.maximize_window()
     firefox_driver.implicitly_wait(5)
     firefox_driver.get('https://app-dev.troops.online/')
-    #firefox_driver.find_element(By.ID,'_username').send_keys('interim_director0@troops.online')
+    #fill Credentials
     WebElements.USERNAME_FIELD.get_web_el(firefox_driver).send_keys(Credentials.username)
-    #firefox_driver.find_element(By.ID,'_password').send_keys('interim_director0@troops.online')
-    WebElements.USERNAME_FIELD.get_web_el(firefox_driver).send_keys(Credentials.userpassword)
-    firefox_driver.find_element(By.XPATH,"//button[@class='primary large']").click()
-    sleep(3)
-    firefox_driver.find_element(By.XPATH, "//li[@data-testid = 'TEACandidatesListLink']").click()
+    WebElements.USERPASSWORD_FIELD.get_web_el(firefox_driver).send_keys(Credentials.userpassword)
+    WebElements.SE_BUTTON.get_web_el(firefox_driver).click() #click on the Se connecter button
+    #click on the Talents link
+    WebElements.TALENTS_LINK.get_web_el(firefox_driver).click()
+    #click on the Add button
     firefox_driver.find_element(By.XPATH, "//app-ui-button-text[@id = 'candidates-list_add-candidate-button']/button[@class='primary large']").click()
+    #click on the Chef actuel
     firefox_driver.find_elements(By.XPATH,'//troops-selector[@class="ng-star-inserted"]')[0].click()
-    assert \
-        firefox_driver.find_elements(By.XPATH,
-                                        "//div[contains(@class,'text') and contains(@class,'ng-tns-c545-1')]")[0].text, 'Add talents'
+    assert WebElements.POPUP_TITLE.get_web_el(firefox_driver)[0].text, 'Add talents'
 
 
